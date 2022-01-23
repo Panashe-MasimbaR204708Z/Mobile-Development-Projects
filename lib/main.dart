9e6aadb75a3972206f6113737d87703934461b2e
Panashe-MasimbaR204708Z <92789827+Panashe-MasimbaR204708Z@users.noreply.github.com>
2022-01-23 14:26:22 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Panashe Masimba R204708Z"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  ));
+}
